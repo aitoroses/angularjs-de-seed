e739f9c73ddb1c80fd886442a922bd89639a5bf3
Robin Böhm <robinboehm@googlemail.com>
2014-04-06 21:10:48 +0200

    feat(module): add module file + test

---
+++ test/unit/module.js
@@ -0,0 +1,10 @@
+describe('An angularjsDE module', function () {
+  it('should exists', function () {
+
+    var loadMainModule = function () {
+      angular.module('angularjsDE');
+    };
+
+    expect(loadMainModule).not.toThrow();
+  });
+});
